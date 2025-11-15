9046e031fe449ce890807bc6de0b2f8336aa9651
Rico Elmerich <rico-elmerich@t-online.de>
2025-11-15 19:42:25 +0100

    feat: add load url function

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,13 @@ let Base_URL = "https://pokeapi.co/api/v2/pokemon/1"
 
 function init() {
     pokemonCardTemplate();
+    loadData();
+    
+}
+
+async function loadData(path= "") {
+    let response = await fetch(Base_URL + path + ".json");
+    let responseToText = await response.text();
+    console.log(responseToText);
+    
 }
