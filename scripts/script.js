0c6f06386d8abcd41ce6b5048408a94d5aeb1010
Rico Elmerich <rico-elmerich@t-online.de>
2025-11-19 14:54:08 +0100

    -feat: change type names to icons

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,57 @@
 let Base_URL = "https://pokeapi.co/api/v2/pokemon/";
 
 let icons = {
-    bug: "icons/bug.svg",
-    dark: "icons/dark.svg",
-    dragon: "icons/dragon.svg",
-    electric: "icons/electric.svg",
-    fairy: "icons/fairy.svg",
-    fighting: "icons/fighting.svg",
-    fire: "icons/fire.svg",
-    flying: "icons/flying.svg",
-    ghost: "icons/ghost.svg",
-    grass: "icons/grass.svg",
-    ground: "icons/ground.svg",
-    ice: "icons/ice.svg",
-    normal: "icons/normal.svg",
-    poison: "icons/poison.svg",
-    psychic: "icons/psychic.svg",
-    rock: "icons/rock.svg",
-    steel: "icons/steel.svg",
-    water: "icons/water.svg"
+  bug: "icons/bug.svg",
+  dark: "icons/dark.svg",
+  dragon: "icons/dragon.svg",
+  electric: "icons/electric.svg",
+  fairy: "icons/fairy.svg",
+  fighting: "icons/fighting.svg",
+  fire: "icons/fire.svg",
+  flying: "icons/flying.svg",
+  ghost: "icons/ghost.svg",
+  grass: "icons/grass.svg",
+  ground: "icons/ground.svg",
+  ice: "icons/ice.svg",
+  normal: "icons/normal.svg",
+  poison: "icons/poison.svg",
+  psychic: "icons/psychic.svg",
+  rock: "icons/rock.svg",
+  steel: "icons/steel.svg",
+  water: "icons/water.svg",
 };
 
 let data = "";
 
- async function init() {
-    
-   await loadData("1");
-    insertTypes();
+async function init() {
+  await loadData("1");
+  insertTypes();
 }
 
-async function loadData(path="") {
-    let response = await fetch(Base_URL + path);
-    let responseAsJSON = await response.json();
-    data = responseAsJSON;
-    
-    console.log(responseAsJSON);
+async function loadData(path = "") {
+  let response = await fetch(Base_URL + path);
+  let responseAsJSON = await response.json();
+  data = responseAsJSON;
 
-    let name = responseAsJSON.forms[0].name
-    let mainRef = document.getElementById('main');
-    let pic = responseAsJSON.sprites.front_default;
+  console.log(responseAsJSON);
 
-    mainRef.innerHTML =  pokemonCardTemplate(name, pic);   
+  let name = responseAsJSON.forms[0].name;
+  let mainRef = document.getElementById("main");
+  let pic = responseAsJSON.sprites.front_default;
+
+  mainRef.innerHTML = pokemonCardTemplate(name, pic);
 }
 
 function insertTypes() {
-    let types = data.types;
-    let cardFooter = document.getElementById('card-footer');
+  let types = data.types;
+  let cardFooter = document.getElementById("card-footer");
 
-    for (let indexTypes = 0; indexTypes < types.length; indexTypes++) {
-        let typeName = types[indexTypes].type.name;
-        cardFooter.innerHTML += typeName;
-    }
+  for (let indexTypes = 0; indexTypes < types.length; indexTypes++) {
+    let typeName = types[indexTypes].type.name;
+    let typeIcon = document.createElement("img");
+
+    typeIcon.src = icons[typeName];
+    typeIcon.classList.add("type-icon");
+    cardFooter.appendChild(typeIcon);
+  }
 }
